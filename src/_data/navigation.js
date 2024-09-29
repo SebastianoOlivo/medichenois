0779b53e7005d2015cbb937964108bdd25cf63ca
sebastiano olivo <olivo.sebastiano@gmail.com>
2024-09-29 20:35:17 +0200

    Improve setup and ready the site for initial phase

---
+++ src/_data/navigation.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    label: "Home",
+    trigger: "home",
+    url: "/"
+  },
+  {
+    label: "Practitioners",
+    trigger: "practitioners",
+    url: "/ractitioners"
+  },
+  {
+    label: "News",
+    trigger: "news",
+    url: "/news"
+  },
+  {
+    label: "Contact",
+    trigger: "ontact",
+    url: "/#Contact"
+  }
+]
+
--- src/_data/site.js
+++ src/_data/site.js
@@ -4,4 +4,5 @@ export default {
   url: "https://www.medichenois.com",
   baseUrl: "/",
   buildTime: new Date(),
+  maintenanceMode: true,
 };
